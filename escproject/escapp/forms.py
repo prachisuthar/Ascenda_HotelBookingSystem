8a70a6044b689e1bac5c0e8a7b0b2fbb3934f248
Prachi Suthar <prachi_suthar@mymail.sutd.edu.sg>
2022-08-04 21:45:48 +0800

    fixed signup

--- escproject/escapp/forms.py
+++ escproject/escapp/forms.py
@@ -1,11 +1,27 @@
 from turtle import textinput
 from django import forms
-from matplotlib import widgets
 from .models import *
 from .views import * 
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 
+# class SignUpForm(forms.ModelForm):
+#     username = forms.CharField(widget=forms.TextInput(attrs={"class": "register-page-textinput input"}))
+#     password = forms.CharField(widget=forms.PasswordInput(attrs={"class": "register-page-textinput input"}))
+#     confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={"class": "register-page-textinput input"}))
+#     email = forms.CharField(widget=forms.EmailInput(attrs={"class": "register-page-textinput input"}))
+#     full_name = forms.CharField(widget=forms.TextInput(attrs={"class": "register-page-textinput input"}))
+
+#     class Meta:
+#         model = Customer
+#         fields = ["username", "password", "full_name", "password", "confirm_password", "email"]
+
+#     def clean_username(self):
+#         uname = self.cleaned_data.get("username")
+#         if User.objects.filter(username=uname).exists():
+#            raise forms.ValidationError("Username already exists. Please choose another username.")
+
+#         return uname 
 class SignUpForm(forms.ModelForm):
     username = forms.CharField(widget=forms.TextInput(attrs={"class": "register-page-textinput input"}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={"class": "register-page-textinput input"}))
@@ -22,8 +38,7 @@ class SignUpForm(forms.ModelForm):
         if User.objects.filter(username=uname).exists():
            raise forms.ValidationError("Username already exists. Please choose another username.")
 
-        return uname 
-    
+        return uname   
 
 class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={"class": "login-page-textinput input"}))
